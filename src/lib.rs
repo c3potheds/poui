34cc1cfa08d9f7425d911b8f07203f30df44b913
Simeon <simeon.anfinrud@mac.com>
2024-05-29 23:24:47 -0700

    Wrapping addition.
    Switch to u32 instead of i32, because we want the wrapping
    of unsigned integers.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,11 @@
-
 #[derive(Debug, PartialEq)]
-pub struct Poui(pub i32);
+pub struct Poui(pub u32);
 
 impl std::ops::Add for Poui {
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self {
-        Poui(self.0 + rhs.0)
+        Poui(self.0.wrapping_add(rhs.0))
     }
 }
 
@@ -15,4 +14,11 @@ fn basic_arithmetic() {
     let a = Poui(1);
     let b = Poui(2);
     assert_eq!(a + b, Poui(3));
+}
+
+#[test]
+fn wraparound() {
+    let a = Poui(u32::MAX);
+    let b = Poui(1);
+    assert_eq!(a + b, Poui(0));
 }
